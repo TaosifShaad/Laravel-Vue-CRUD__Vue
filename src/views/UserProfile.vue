fdffa64d56ac28a8c85db62abea1a2a713ead0e4
Taosif <taosif@softzino.com>
2023-01-03 13:36:34 +0600

    update profile

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="profile" :class="theme.themeBtn && 'night-profile'">
         <h3>name: {{ state.user.name }}</h3>
-        <h3>mail: {{ state.user.email }}</h3>
+        <h3>email: {{ state.user.email }}</h3>
         <h3>Account created at: {{ new Date(state.user.created_at).toLocaleString('en-GB', { timeZone: 'UTC' }) }}</h3>
         <form @submit.prevent="deleteAcc" action="">
             <input name="id" :value="state.id.key" type="hidden" />
-            <button class="btn btn-danger mt-4">Delete account</button>
+            <div>
+                <modal>
+                    <template #input>
+                        <input v-model="state.newProfile.name" type="text" placeholder="name" name="name"> <br>
+                        <input v-model="state.newProfile.email" type="text" placeholder="email" name="email">
+                    </template>
+                    <template #submit>
+                        <button @click="updateProfile" type="button" class="btn btn-primary">Submit</button>
+                    </template>
+                </modal>
+                <button class="btn btn-danger mt-4">Delete account</button>
+            </div>
         </form>
     </div>
 </template>
@@ -18,11 +29,17 @@ import router from '@/router';
 import { createToaster } from "@meforma/vue-toaster";
 import Swal from 'sweetalert2';
 import { themeToggle } from '@/stores/themeStore';
+import modal from '@/components/modal.vue'
 
 const state = reactive({
     user: storage.getItem('user'),
     id: {
         key: storage.getItem('user').id
+    },
+    newProfile: {
+        name: '',
+        email: '',
+        id: storage.getItem('user').id
     }
 });
 
@@ -37,7 +54,6 @@ function deleteAcc() {
         confirmButtonText: 'yes',
         denyButtonText: `no`,
     }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
             axios.post("/delete", state.id)
                 .then(
@@ -61,6 +77,36 @@ function deleteAcc() {
         })
     });
 }
+
+function updateProfile() {
+    axios.post("/updateProfile", state.newProfile)
+        .then(
+            ({ data }) => {
+                console.log(data);
+                storage.setItem('user', data);
+                Swal.fire({
+                    title: 'Updated profile!',
+                    icon: 'success',
+                    confirmButtonText: 'close',
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        router.go();
+                    }
+                })
+            }
+        ).catch(error => {
+            toaster.error(error.message, {
+                position: 'top-right'
+            })
+        });
+}
+
+// watch(state, newValue => {
+//     // if (state.newProfile != newValue.newProfile) {
+//         console.log('watch');
+//         state.newProfile == newValue;
+//     // }
+// });
 </script>
 
 <style scoped>
@@ -73,12 +119,14 @@ function deleteAcc() {
     background: #f1f1f1;
     box-shadow: 0px 0px 11px 0px grey;
 }
+
 .night-profile {
     background: black;
     color: antiquewhite;
     box-shadow: 0px 0px 25px 4px black;
     transition: background 0.5s, color 0.5s, box-shadow 0.5s;
 }
+
 div:not(.night-profile) {
     transition: 0.5s;
 }
